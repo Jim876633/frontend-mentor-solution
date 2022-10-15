ff1c9aebaf76605faab0abd1c105c1d98ecb4cac
jim <jim30223@gmail.com>
2022-10-15 16:40:37 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,77 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
+
+Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+-   [Overview](#overview)
+    -   [The challenge](#the-challenge)
+    -   [Screenshot](#screenshot)
+    -   [Links](#links)
+-   [My process](#my-process)
+    -   [Built with](#built-with)
+    -   [What I learned](#what-i-learned)
+    -   [Useful resources](#useful-resources)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+-   View the optimal layout for each page depending on their device's screen size
+-   See hover states for all interactive elements on the page
+-   See their own IP address on the map on the initial page load
+-   Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+Mobile
+
+![](/screenshot/desktop.jpeg)
+
+Desktop
+
+![](/screenshot//mobile.jpeg)
+
+### Links
+
+-   Solution URL: [fontend mentor solution]()
+-   Live Site URL: [live site]()
+
+## My process
+
+### Built with
+
+-   SCSS
+-   Mobile-first workflow
+-   [Ipify](https://www.ipify.org/) - IP API
+-   [Ipify geolocation](https://geo.ipify.org/) - IP Geolocation API
+-   [Leaflet](https://leafletjs.com/) - Map API
+
+### What I learned
+
+use Leaflet API to create the map.
+
+```js
+const map = L.map("map");
+map.setView(coords, 13);
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+const icon = L.icon({
+    iconUrl: "/images/icon-location.svg",
+    iconSize: [36, 46],
+});
+var marker = L.marker(coords, { icon }).addTo(map);
+marker.bindPopup("<b>Hello vistor !</b><br>IP address is here.").openPopup();
+```
+
+### Useful resources
+
+-   [IP RegExp](https://www.regextester.com/104038m) - This helped me to filter correct IP before fetch API.
---
+++ app.js
@@ -0,0 +1,130 @@
+const ipAPI_KEY = "at_NpVUw6cctJ6nVgjlbhVr30nyEfCJG";
+
+const formEl = document.querySelector(".search__form");
+
+const inputEl = formEl.querySelector("input");
+
+const map = L.map("map");
+
+//================================================//
+//Get Local IP address
+//================================================//
+
+const getIP = (json) => {
+    fetchLocation(json.ip);
+};
+
+//================================================//
+//Loaction result render
+//================================================//
+
+const renderResult = (result) => {
+    Object.entries(result).forEach(([key, value]) => {
+        const resultEl = document.querySelector(`.search__result--${key}`);
+        resultEl.querySelector(".result").textContent = value;
+    });
+    inputEl.value = "";
+};
+
+const inValidAlert = () => {
+    formEl.classList.remove("valid");
+    formEl.classList.add("inValid");
+    inputEl.value = "";
+};
+
+const validAlert = () => {
+    formEl.classList.remove("inValid");
+    formEl.classList.add("valid");
+};
+const blurInput = () => {
+    formEl.classList.remove("inValid");
+    formEl.classList.remove("valid");
+};
+
+//================================================//
+//Leaf map render
+//================================================//
+
+const renderMap = (coords) => {
+    map.setView(coords, 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    const icon = L.icon({
+        iconUrl: "/images/icon-location.svg",
+        iconSize: [36, 46],
+    });
+    var marker = L.marker(coords, { icon }).addTo(map);
+    marker
+        .bindPopup("<b>Hello vistor !</b><br>IP address is here.")
+        .openPopup();
+};
+
+//================================================//
+//fetch Location
+//================================================//
+
+const fetchLocation = async (ip) => {
+    const ipAPI_URL = `https://geo.ipify.org/api/v2/country,city?apiKey=${ipAPI_KEY}&ipAddress=${ip}`;
+    const res = await fetch(ipAPI_URL);
+    const data = await res.json();
+    if (data.code == 422) {
+        inValidAlert();
+        return;
+    }
+    const result = {
+        ip: data.ip,
+        location: `${data.location.city},${data.location.region},${data.location.country}`,
+        timezone: `UTC${data.location.timezone}`,
+        isp: data.isp,
+    };
+    const coords = [data.location.lat, data.location.lng];
+    renderResult(result);
+    renderMap(coords);
+};
+//================================================//
+//Form submit
+//================================================//
+
+submitHandler = (e) => {
+    e.preventDefault();
+    const inputValue = inputEl.value;
+    const ipRegExp =
+        /((^\s*((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*$)|(^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$))/g;
+
+    if (inputValue.match(ipRegExp)) {
+        fetchLocation(inputValue);
+    } else {
+        inValidAlert();
+    }
+};
+
+//================================================//
+//Event Listener
+//================================================//
+
+["DOMContentLoaded", "resize"].forEach((event) => {
+    window.addEventListener(event, () => {
+        const search = document.querySelector(".search");
+        search.style.height = "fit-content";
+        const map = document.querySelector("#map");
+
+        const searchHeight = search.clientHeight;
+
+        const resultHeight =
+            document.querySelector(".search__result").clientHeight;
+
+        const newSearchHeight = searchHeight - resultHeight / 2;
+
+        search.style.height = newSearchHeight + "px";
+        map.style.height = `calc(100vh - ${newSearchHeight}px)`;
+    });
+});
+
+formEl.addEventListener("submit", submitHandler);
+
+inputEl.addEventListener("input", validAlert);
+inputEl.addEventListener("blur", blurInput);
