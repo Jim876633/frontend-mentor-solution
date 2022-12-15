ebdf0627e1f5b1f341728d68eab5362a98f5370a
jim <jim30223@gmail.com>
2022-12-15 23:03:23 +0800

    feat: add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,24 @@
+import classes from "./index.module.scss";
+
+const Footer = () => {
+    return (
+        <footer>
+            <p class={classes.footer}>
+                Challenge by
+                <a
+                    href="https://www.frontendmentor.io?ref=challenge"
+                    target="_blank"
+                >
+                    Frontend Mentor
+                </a>
+                . Coded by{" "}
+                <a href="https://github.com/Jim876633" target="_blank">
+                    Jim Huang
+                </a>
+                .
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
